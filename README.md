bdb897bd4ac783f4b4d2a67bffb6bd83fed68c22
Andy Gimblett <andy@barefootcode.com>
2024-03-19 08:30:35 +0000

    Stop setting tab titles directly: just set an env var

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Andy's zsh plugin for setting iTerm2 titles and colors
+# Andy's zsh plugin for setting iTerm2 tab colors and title overrides
 
-This plugin provides five functions for setting an iTerm2 tab's title/session name, and color:
+This plugin provides five functions for setting an iTerm2 tab's color and a title override:
 
-* `iterm2_tab_title` with alias `tt`
 * `iterm2_tab_color` with alias `tc`
 * `iterm2_tab_color_named` with alias `tcn`
 * `iterm2_tab_color_random` with alias `tcr`
 * `iterm2_tab_color_random_named` with alias `tcnr`
+
+and:
+
+* `iterm2_tab_title` with alias `tt`
 
 See [`iterm2-tabs.zsh`](iterm2-tabs.zsh) for more details, and examples.
 
@@ -17,9 +20,6 @@ Colors may be set either as RGB triples or as named colors, where the list of co
 
 Most of the color-related work is done by [a python script](iterm2_tabs.py).
 
-Note that to set tab titles using this plugin you do need to enable the `Applications in terminal may change the title` option on your iTerm2 profile, allowing the title to be set by `echo`s of [proprietary escape codes](https://www.iterm2.com/documentation-escape-codes.html). This does unfortunately mean that other processes that do that same can then overwrite the title you've chosen, but this is the simplest and most reliable way to make this happen.  I experimented for a while with using the iTerm2 python API to set the session name, which is nicely "sticky", but since the 3.3.9 release the security model made that more inconvenient than seemed worthwhile, so I've reverted to the escape code technique.
+Note that (unlike previous versions of this plugin), some external machinery is needed to actually set tab titles using the function provided here; see comments in the plugin itself for more details.
 
-If you want to set tab title and/or color automatically when you enter some directory, try
-[Tarrasch/zsh-autoenv](https://github.com/Tarrasch/zsh-autoenv) in conjunction with this plugin.
-
-Andy Gimblett, andy@barefootcode.com. 2017-2020
+Andy Gimblett, <andy@barefootcode.com>. 2017-2024
--- iterm2-tabs.zsh
+++ iterm2-tabs.zsh
@@ -1,74 +1,45 @@
-# zsh plugin for setting iTerm2 tab/window titles and tab colors
+# zsh plugin for setting iTerm2 tab colors and title overrides
 #
 # Andy Gimblett, 2017-2020
 #
-# This provides the following six functions (and their aliases):
+# This provides the following five functions (and their aliases):
 #
-# iterm2_window_title           (wt)
-# iterm2_tab_title              (tt)
 # iterm2_tab_color              (tc)
 # iterm2_tab_color_named        (tcn)
 # iterm2_tab_color_random       (tcr)
 # iterm2_tab_color_random_named (tcnr)
 #
-# Setting the window or tab title is performed in a straightforward manner via
-# an echo with some control codes; there are two prerequisites for this to
-# work:
+# # iterm2_tab_title              (tt)
 #
-# 1. The option "Applications in terminal may change the title" option
-#    must be ticked in your iTerm2 profile's "General" preferences
-#    tab. You need to ensure this.
 #
-# 2. For tab titles: the DISABLE_AUTO_TITLE env var must be set to "true"; this
-#    prevents iTerm2 from automatically overwriting the tab title with a
-#    session name, job name, etc. This env var is set for you automatically by
-#    the iterm2_tab_title function, so you don't need to do anything here
-#    except be aware that those automatic updates won't be happening once you
-#    explicitly define a title - which is presumably what you wanted to have
-#    happen. :-)
-#
-# All the colour-related commands are handled by a python script (in
-# the same directory).
+# All the colour-related commands are handled by a python script (in the same
+# directory).
 #
 # The colors are taken from
 # https://github.com/jacaetevha/finna-be-octo-hipster; I didn't use
 # that directly because I didn't like having one function per color.
 #
-# If you want to set title/color automatically when you enter some
-# directory, these functions work well in conjunction with the
-# Tarrasch/zsh-autoenv plugin.
+#
+# Note that `iterm2_tab_title` simply sets a "tab title override" env var, and
+# on its own won't actually affect the tab title.  Previous versions of this
+# plugin used iterm2 proprietary escape codes to set the tab title, but I now
+# use a more complex (and more powerful) approach to set my tab titles, which
+# involves two further steps (not included in this plugin and left as an
+# exercise for the reader):
+#
+# 1. An `iterm2_print_user_vars()` checks if the tab title override env var is
+#    set, and if so, copies its value to an iterm2 user variable.
+# 2. An AutoLaunch script computes the tab title: if the override is set, it
+#    uses that, otherwise it uses some internal logic to automatically choose a
+#    title to my preference. (This script is the reason for the change in
+#    approach, basically.)  My iterm2 profile is then configured to set the tab
+#    title via this scrpt.
+
 
 # We expect the python script to be in the same folder as the script
 # you're reading now
 #
 _iterm2_tabs_py=${0:a:h}/iterm2_tabs.py
-
-# Set window title, e.g.
-#
-# $ iterm2_window_title hello
-# $ iterm2_window_title Long titles OK
-#
-# https://www.reddit.com/r/zsh/comments/jp18n3/how_to_rename_iterm2_window_title_with_zsh/gbdj1et/
-#
-iterm2_window_title () {
-    export DISABLE_AUTO_TITLE="true"
-    echo -ne "\e]2;$*\a"
-}
-alias wt=iterm2_window_title
-
-
-# Set tab title, e.g.
-#
-# $ iterm2_tab_title hello
-# $ iterm2_tab_title Long titles OK
-#
-# # https://www.reddit.com/r/zsh/comments/jp18n3/how_to_rename_iterm2_window_title_with_zsh/gbdj1et/
-#
-iterm2_tab_title () {
-    export DISABLE_AUTO_TITLE="true"
-    echo -ne "\e]1;$*\a"
-}
-alias tt=iterm2_tab_title
 
 
 # Set tab color with r g b triple, e.g.
@@ -128,3 +99,17 @@ _tab_color_completion() {
     _values $($_iterm2_tabs_py --list-colors)
 }
 compdef _tab_color_completion iterm2_tab_color_named
+
+
+# Set tab title override env var, e.g.
+#
+# $ iterm2_tab_title hello
+# $ iterm2_tab_title Long titles OK
+#
+# See the comment at the top of this file for more info on what's needed to
+# actually make this change the tab title.
+#
+iterm2_tab_title () {
+    export TAB_TITLE_OVERRIDE="$*"
+}
+alias tt=iterm2_tab_title
